596538b591978dc9f78396238b76db54318a77e7
melaniecristeche <u943142818@byui.edu>
2022-03-23 16:09:21 -0300

    director merged

--- game/directing/director.py
+++ game/directing/director.py
@@ -1,47 +1,82 @@
-from game.services.video_service import VideoService
-from game.services.keyboard_service import KeyboardService
+import random
+from game.point import Point
+
+
 class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
+        _keyboard_service (KeyboardService): For getting directional input.
         _video_service (VideoService): For providing video output.
     """
 
-    def __init__(self, video_service):
-        """Constructs a new Director using the specified video service.
+    def __init__(self, keyboard_service, video_service):
+        """Constructs a new Director using the specified keyboard and video services.
         
         Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
             video_service (VideoService): An instance of VideoService.
         """
+        self._keyboard_service = keyboard_service
         self._video_service = video_service
-        self._keyboard_service=KeyboardService()
-        self._Is_Loading=True
         
-    def start_game(self, cast, script):
-        """Starts the game using the given cast and script. Runs the main game loop.
+    def start_game(self, cast):
+        """Starts the game using the given cast. Runs the main game loop.
 
         Args:
             cast (Cast): The cast of actors.
-            script (Script): The script of actions.
         """
-        
-        self._video_service.open_window()    
+        self._video_service.open_window()
         while self._video_service.is_window_open():
-            self._execute_actions("input", cast, script)
-            self._execute_actions("update", cast, script)
-            self._execute_actions("output", cast, script)
+            self._get_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
         self._video_service.close_window()
 
-    def _execute_actions(self, group, cast, script):
-        """Calls execute for each action in the given group.
+    def _get_inputs(self, cast):
+        """Gets directional input from the keyboard and applies it to the robot.
         
         Args:
-            group (string): The action group name.
             cast (Cast): The cast of actors.
-            script (Script): The script of actions.
         """
-        actions = script.get_actions(group)    
-        for action in actions:
-            action.execute(cast, script)          
+        robot = cast.get_first_actor("robots")
+        velocity = self._keyboard_service.get_direction()
+        robot.set_velocity(velocity)        
+
+    def _do_updates(self, cast):
+        """Updates the robot's position and resolves any collisions with artifacts.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        banner = cast.get_first_actor("banners")
+        robot = cast.get_first_actor("robots")
+        artifacts = cast.get_actors("artifacts")
+
+        banner.set_text("")
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        robot.move_next(max_x, max_y)
+        
+        for artifact in artifacts:
+            if robot.get_position().equals(artifact.get_position()):
+                message = artifact.get_message()
+                banner.set_text(message)   
+                # x = random.randint(1, 60 - 2)
+                # y = random.randint(1, 40 - 2)
+                # position = Point(x, y)
+                # position = position.scale(15)
+                # artifact.set_position(position) 
+        
+    def _do_outputs(self, cast):
+        """Draws the actors on the screen.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.clear_buffer()
+        actors = cast.get_all_actors()
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
